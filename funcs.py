8690aa84ed687931e080bc0957da697ff1eeafae
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-05-06 02:35:28 +0300

    rename total -> total_numbers, add subtruct_numbers func

--- funcs.py
+++ funcs.py
@@ -1,4 +1,4 @@
-def total(a: int, b: int) -> int:
+def total_numbers(a: int, b: int) -> int:
     """
     Args:
         a: int
@@ -6,3 +6,13 @@ def total(a: int, b: int) -> int:
     Return: int, add a + b
     """
     return a + b
+
+
+def subtruct_numbers(a: int, b: int) -> int:
+    """
+    Args:
+        a: int
+        b: int
+    Return: int, sub a - b
+    """
+    return a - b
